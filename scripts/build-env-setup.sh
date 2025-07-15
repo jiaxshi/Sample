9abe447ad1957c462dc7bb1ca4df2e760a14db67
Jiaxing Shi <jiaxshi@qti.qualcomm.com>
2025-07-15 16:36:33 +0800

    add update

--- scripts/build-env-setup.sh
+++ scripts/build-env-setup.sh
@@ -30,6 +30,8 @@ rosdep update
 # Temp link for debug purpose
 sudo wget https://github.com/qualcomm-qrb-ros/qrb_ros_distro/blob/080a6ba7bbcd335736335125f11cdd31c05c9f54/jazzy/qcom-distribution.yaml -O /etc/ros/rosdep/sources.list.d/qcom-distribution.yaml
 echo "yaml file:///etc/ros/rosdep/sources.list.d/qcom-distribution.yaml" | sudo tee -a /etc/ros/rosdep/sources.list.d/20-default.list
+rosdep update
+sudo apt install qcom-camx
 
 # Install required dependencies
 rosdep install -y --rosdistro "$ROS2_DISTRO" --from-paths ${SRC_DIR} --ignore-src
